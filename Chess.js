25bc8a85b205d1468aed12189dd74887e6009359
mubasher713 <mubasher713@gmail.com>
2019-05-02 13:45:13 -0400

    Improve Chess Puzzle
    Add more puzzles and difficulties

--- Chess.js
+++ Chess.js
@@ -15,17 +15,86 @@ var bP = "&#9823;"
 var game
 var gameAnswer
 var gameComp
-var easyPieceLocations = [['a8.' + wB, 'h8.' + bK, 'a7.' + bP, 'h7.' + bP, 'b6.' + bP, 'd6.' + wP, 'e6.' + wQ, 'c5.' + bP, 'e5.' + bB, 'h4.' + wP, 'b3.' + wP, 'g3.' + bQ, 'a2.' + wP, 'f1.' + wR, 'h1.' + wK ]]
-var easyAnswer = [['f1f8', 'f8g8']]
-var easyCompMove = [['h8g7']]
+var easyPieceLocations = [['a8.' + wB, 'h8.' + bK, 'a7.' + bP, 'h7.' + bP, 'b6.' + bP, 'd6.' + wP, 'e6.' + wQ, 'c5.' + bP, 'e5.' + bB, 'h4.' + wP, 'b3.' + wP, 'g3.' + bQ, 'a2.' + wP, 'f1.' + wR, 'h1.' + wK ], 
+['b8.' + bK, 'a7.' + wR, 'b7.' + wH, 'c7.' + bB, 'a6.' + wK, 'c6.' + wR]]
+var easyAnswer = [['f1f8', 'f8g8'], ['b7c5', 'c5d7']]
+var easyCompMove = [['h8g7'], ['c7f4']]
+
+var mediumPieceLocations = [['c8.'+ bK, 'd8.' + bR, 'f8.' + bR, 'a7.' + bP, 'e7.' + wR,
+'f7.' + wQ, 'b6.' + bP, 'd6.' + bP, 'h6.' + bP, 'c3.' + wP, 'a2.' + wP, 'b2.' + wP, 'f2.' + wP,
+'g2.' + wP, 'h2.' + wP, 'a1.' + wR, 'b1.' + wH, 'g1.'+ wK]]
+var mediumAnswer = [['f7c4', 'c4c7', 'c7b7']]
+var mediumCompMove = [['c8b8', 'b8a8']]
+
+var hardPieceLocations = [['f8.' + bR, 'g8.' + bK, 'd7.' + bB, 'f7.' + bP, 'g7.' + bP,
+'h7.' + bP, 'a5.' + bP, 'b5.' + bQ, 'd5.' + wH, 'b4.' + bP, 'g4.' + wP, 'c3.' + wP, 'd3.' + wQ,
+'f3.' + wP, 'a2.' + wP, 'b2.' + wP, 'b1.' + wK, 'e1.' + wR]]
+var hardAnswer = [['d5e7', 'd3h7', 'e1h1', 'h1h5']]
+var hardCompMove = [['g8h8', 'h8h7', 'b5h5']]
+
 var rand
 var moveNumber = 0
-function run()
+
+function reset()
+{
+    locations = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    locations2 = ['1', '2', '3', '4', '5', '6', '7', '8']
+    for (i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 8; j++)
+        {
+            temp = locations[i] + locations2[j]
+            document.getElementById(temp).innerHTML = ''
+        }
+    }
+}
+
+function runEasy()
 {
     rand = Math.floor(Math.random() * easyPieceLocations.length)
     game = easyPieceLocations[rand]
     gameAnswer = easyAnswer[rand]
     gameComp = easyCompMove[rand]
+    elementID = ""
+    piece = ""
+    temp = ""
+    for (i = 0; i < game.length; i++)
+    {
+        temp = game[i].split(".")
+        elementID = temp[0]
+        piece = temp[1]
+        document.getElementById(elementID).innerHTML = piece
+        
+        document.getElementById(elementID).fontSize = 9
+    }
+}
+
+function runMedium()
+{
+    rand = Math.floor(Math.random() * mediumPieceLocations.length)
+    game = mediumPieceLocations[rand]
+    gameAnswer = mediumAnswer[rand]
+    gameComp = mediumCompMove[rand]
+    elementID = ""
+    piece = ""
+    temp = ""
+    for (i = 0; i < game.length; i++)
+    {
+        temp = game[i].split(".")
+        elementID = temp[0]
+        piece = temp[1]
+        document.getElementById(elementID).innerHTML = piece
+        
+        document.getElementById(elementID).fontSize = 9
+    }
+}
+
+function runHard()
+{
+    rand = Math.floor(Math.random() * hardPieceLocations.length)
+    game = hardPieceLocations[rand]
+    gameAnswer = hardAnswer[rand]
+    gameComp = hardCompMove[rand]
     elementID = ""
     piece = ""
     temp = ""
