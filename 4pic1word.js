58fd33f92796293ca2b2d364b8edecb905e9143e
Bryan Hua <bryanhua88@gmail.com>
2019-05-20 18:35:21 -0400

    Background, Strike, Difficulties

--- 4pic1word.js
+++ 4pic1word.js
@@ -3,17 +3,17 @@ var picsEasy = [["https://grandbaby-cakes.com/wp-content/uploads/2014/09/Fish-an
 	["https://upload.wikimedia.org/wikipedia/commons/8/8b/Moose_superior.jpg","https://i5.walmartimages.com/asr/1313c809-deef-4320-bae7-b974ad6a3e6a_2.930e3b520f82a5eb8a628d073bf2c203.jpeg?odnHeight=60&odnWidth=75&odnBg=FFFFFF","https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Forests%20WB.jpeg?itok=Qk9Pndtv","https://www.gannett-cdn.com/-mm-/2425a0470766b558967df12696ca47cade450c8d/c=149-99-2888-1647/local/-/media/2018/01/20/INGroup/Indianapolis/636520357268068995--us-images-Tim-Hortons-Exterior-2.jpg?width=3200&height=1680&fit=crop","Canada"],
 	["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Mao_Tse-tung_-_panoramio.jpg/220px-Mao_Tse-tung_-_panoramio.jpg","https://www.snopes.com/tachyon/2018/07/great_wall_of_china.jpg?resize=865,452","https://www.fifteenspatulas.com/wp-content/uploads/2012/03/Fried-Rice-Fifteen-Spatulas-8-640x427.jpg","https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.fit-760w.jpg","China"],
 	["https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1487701021/eiffel-tower-paris-france-EIFFEL0217.jpg?itok=m0MZOYjh","https://d2gk7xgygi98cy.cloudfront.net/8-3-large.jpg","https://media-cdn.tripadvisor.com/media/photo-s/05/6b/c7/ee/musee-du-louvre.jpg","https://www.napoleon.org/wp-content/themes/napoleon/images/template/2-empires/napoleon-1er.jpg","France"],
-	["https://i.pinimg.com/originals/61/1d/82/611d82f380cfb21a8ee620a60d28feb3.jpg","https://previews.123rf.com/images/gaschwald/gaschwald1307/gaschwald130700107/21119193-side-view-of-a-deactivated-sa-5b-gammon-s-200m-vega-m-surface-to-air-missile-sam-as-a-war-monument-i.jpg","https://retrothing.typepad.com/photos/uncategorized/appleii.jpg","http://www.asianews.it/files/img/COREA_-_Dialoghi.jpg","North Korea"],
 	["https://www.corriecooks.com/wp-content/uploads/2018/09/Instant-Pot-Beef-Coconut-Curry.jpg","https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg","https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-0-20151104113424.jpg","https://pbs.twimg.com/profile_images/843080269091495936/4IUdujgj_400x400.jpg","India"],
 	["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStWDMeYY1fjQ_aP-Wb8jN68StOEvjljnYEhiwt6q8DQG4pZwbp","https://feelingfoodish.com/wp-content/uploads/2012/08/New-York-Style-pizza.jpg","https://www.worldatlas.com/upload/e8/ad/89/italian-peninsula.jpg","https://ih1.redbubble.net/image.366998385.6682/flat,550x550,075,f.jpg","Italy"],
+	["https://lh3.googleusercontent.com/yAtZnNL-9Eb5VYSsCaOC7KAsOVIJcY8mpKa0MoF-0HCL6b0OrFcBizURHywpuip-D6Y","https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg","https://www.cimt.org.uk/projects/mepres/book7/bk7i22/s2eg2.gif","https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1200px-Square_-_black_simple.svg.png","cube"],
 	["https://www.history.com/.image/t_share/MTU3ODc5MDg2NDMxODcyNzM1/egyptian-pyramids-hero.jpg","https://img.freepik.com/free-vector/egyptian-hieroglyphics-background-with-flat-design_23-2147882324.jpg?size=338&ext=jpg","https://www.worldatlas.com/r/w728-h425-c728x425/upload/32/fb/8f/shutterstock-511451053.jpg","https://vignette.wikia.nocookie.net/omniversal-battlefield/images/4/44/Ra_%28Egyptian_Mythology%29.jpg/revision/latest?cb=20180622041455","Egypt"],
 	["http://discovermagazine.com/~/media/Images/Issues/2016/October/20-things-bats.jpg","https://cdn.shopify.com/s/files/1/1150/3180/products/b45-custom-trophy-baseball-bat-custom-trophy-baseball-bat-1186957852701.jpg?v=1510245868","https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Marcus_Thames_Tigers_2007.jpg/1200px-Marcus_Thames_Tigers_2007.jpg","https://upload.wikimedia.org/wikipedia/en/8/87/Batman_DC_Comics.png","Bat"],
 	["https://media.idownloadblog.com/wp-content/uploads/2016/09/iOS-9-Mail-app-icon-full-size.jpg","https://images.homedepot-static.com/productImages/0b798786-7abc-4471-a81d-405ef49f195f/svn/architectural-mailboxes-mailbox-post-kits-7680b-10-64_1000.jpg","https://176g4u2eqkgm30b0371yje33-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/image-11.png","https://akns-images.eonline.com/eol_images/Entire_Site/2016107/rs_1024x759-161107120558-1024AOL.jpg?fit=inside|900:auto&output-quality=90","mail"]]
 
 var picsMedium = [["http://blogs.discovermagazine.com/gnxp/files/2013/06/800px-Landing_of_Columbus_2.jpg","http://www.igme.es/TopoIberia/img/fig/iberia-physical-map.png","https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Matador.JPG/1200px-Matador.JPG","https://vignette.wikia.nocookie.net/montypython/images/f/ff/Spanish_Inquisition.jpg/revision/latest?cb=20180629171423","Spain"],
 	["http://clipart-library.com/images/BigAok9AT.png","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZyGu3mPtNqpK69JsKtIL4pdz_628XTthcgBVFR3m3AMbKT4CM","https://pixel.nymag.com/imgs/daily/vulture/2017/10/10/10-anime-netflix.w700.h700.jpg","https://usercontent2.hubstatic.com/9025907_f520.jpg","Japan"],
-	["https://s26162.pcdn.co/wp-content/uploads/2018/03/Gulag-Map.jpg","http://siberiantimes.com/PICTURES/OTHERS/Franz-Klintsevich-recommends-Siberia/inside_altai.jpg","https://cdn-images-1.medium.com/max/1200/1*L_uBUcOn1-NvGPQFZ4XlrQ.png","https://static.tvtropes.org/pmwiki/pub/images/stalin.jpg","Soviet Union"],
 	["https://i.pinimg.com/originals/11/e1/13/11e1137ae55a4270227b0e62bfa015a9.jpg","https://santorinidave.com/files/2017/10/best-greek-islands-santorini.jpg","https://www.dummies.com/wp-content/uploads/greek-mythology-names.jpg","https://www.maniq.org/wp-content/uploads/2016/12/parthenon.jpg","Greece"],
+	["https://images-na.ssl-images-amazon.com/images/I/81biK28tbiL._SX425_.jpg","https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","https://desight.pro/upload/img/linerix-free/21-577ac11c4668d-big.png","https://media.timeout.com/images/101899943/image.jpg","bar"],
 	["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdUDBuLvgDAU5mKpnQ20G2Hca0QVmsLaKCW39RCiMdxbsZRPA","https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Mitteleuropa_zur_Zeit_der_Staufer.svg/250px-Mitteleuropa_zur_Zeit_der_Staufer.svg.png","http://www.car-brand-names.com/wp-content/uploads/2015/03/German-car-brands-logotypes.png","https://img.rezdy.com/PRODUCT_IMAGE/28281/092014oktoberfest_1280x720_lg.jpg","Germany"],
 	["https://cosmos-images2.imgix.net/file/spina/photo/16745/181005_kangaroo_1.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=835","https://s.yimg.com/ny/api/res/1.2/FZTgC7Gn8N2L5PTnvDG9Cg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://img.huffingtonpost.com/asset/5cb55d09240000030a4f6af8.jpeg","https://s1.at.atcdn.net/wp-content/uploads/2012/10/Driving-Kata-Tjuta.jpg","https://fortunedotcom.files.wordpress.com/2019/02/steve-irwin.jpg","Australia"],
 	["https://ritzcarlton-h.assetsadobe.com/is/image/content/dam/the-ritz-carlton/hotels/mexico-caribbean-and-latin-america/aruba/aruba/activities/supporting-images/RCARUBA_00081_conversion.png?$XlargeViewport100pct$","https://image.shutterstock.com/image-illustration/money-bills-3d-illustration-isolated-260nw-572018755.jpg","https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2018/aguidetopoke.jpg","https://upload.wikimedia.org/wikipedia/commons/5/58/AcetoFive.JPG","Gambling"],
@@ -29,13 +29,13 @@ var picsHard = [["http://2.bp.blogspot.com/-9hhkQf2zocA/Utg0vj3tsjI/AAAAAAAAIfE/
 	["http://sacred-valley-salt.com/wp-content/uploads/2016/10/Khewra-Mosque.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg/220px-Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg","https://i.forbesimg.com/media/lists/people/malala-yousafzai_416x416.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Jinnah1945c.jpg/220px-Jinnah1945c.jpg","Pakistan"],
 	["https://marketingland.com/wp-content/ml-loads/2014/08/amazon-circles-1920.png","https://foundtheworld.com/wp-content/uploads/2015/12/Amazon-Rainforest-1.jpg","https://cdn2.wanderlust.co.uk/media/7872/lists-8-incredible-animals-in-brazils-amazon-rainforest.jpg?anchor=center&mode=crop&width=600&height=225&rnd=131481251940000000","https://images-na.ssl-images-amazon.com/images/I/61eAq6gg-XL._SL1000_.jpg","Amazon"],
 	["https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2018/02/04/zach-ertz-super-bowl-touchdown-catch.png?itok=CVZsogQb","http://www.printable-alphabets.com/wp-content/uploads/2012/10/number-solid-6.jpg","http://static.nfl.com/static/content/public/pg-photo/2018/03/06/0ap3000000919883/ed-hochuli_pg_600.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Patriots-Browns_2007.jpg/350px-Patriots-Browns_2007.jpg","Touchdown"],
-	["https://images-na.ssl-images-amazon.com/images/I/81biK28tbiL._SX425_.jpg","https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","https://desight.pro/upload/img/linerix-free/21-577ac11c4668d-big.png","https://media.timeout.com/images/101899943/image.jpg","bar"],
+	["https://s26162.pcdn.co/wp-content/uploads/2018/03/Gulag-Map.jpg","http://siberiantimes.com/PICTURES/OTHERS/Franz-Klintsevich-recommends-Siberia/inside_altai.jpg","https://cdn-images-1.medium.com/max/1200/1*L_uBUcOn1-NvGPQFZ4XlrQ.png","https://static.tvtropes.org/pmwiki/pub/images/stalin.jpg","Soviet Union"],
 	["https://upload.wikimedia.org/wikipedia/commons/b/bf/Inveraray_Castle_-_south-west_facade.jpg","https://customwagonwheels.com/wp-content/uploads/2014/06/Cannon-Wheels-light-hub.jpg","https://media.gettyimages.com/photos/seige-of-paris-by-the-normans19th-century-colour-lithograph-private-picture-id463906529","https://c8.alamy.com/comp/BPA49T/seige-scene-the-alamo-1960-BPA49T.jpg","seige"],
-	["https://lh3.googleusercontent.com/yAtZnNL-9Eb5VYSsCaOC7KAsOVIJcY8mpKa0MoF-0HCL6b0OrFcBizURHywpuip-D6Y","https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg","https://www.cimt.org.uk/projects/mepres/book7/bk7i22/s2eg2.gif","https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1200px-Square_-_black_simple.svg.png","cube"],
+	["https://i.pinimg.com/originals/61/1d/82/611d82f380cfb21a8ee620a60d28feb3.jpg","https://previews.123rf.com/images/gaschwald/gaschwald1307/gaschwald130700107/21119193-side-view-of-a-deactivated-sa-5b-gammon-s-200m-vega-m-surface-to-air-missile-sam-as-a-war-monument-i.jpg","https://retrothing.typepad.com/photos/uncategorized/appleii.jpg","http://www.asianews.it/files/img/COREA_-_Dialoghi.jpg","North Korea"],
 	["https://m.media-amazon.com/images/S/aplus-media/vc/c828c45e-cae2-47d5-a044-a5a386212cd7._CR6,0,738,984_PT0_SX300__.jpg","https://www.ushotairballoon.com/wp-content/uploads/2018/05/chester-county-hot-air-balloon-ride.jpg","https://previews.123rf.com/images/blueringmedia/blueringmedia1509/blueringmedia150900663/44952954-periodic-symbol-and-diagram-of-helium-illustration.jpg","https://images-na.ssl-images-amazon.com/images/I/81EHUOWBAWL._SY355_.jpg","Balloon"]]
 
 var word
-
+var strike = 0
 /*function run(guess, correctWord)
 {
 	
@@ -49,6 +49,7 @@ function restart()
 	document.getElementById("answerButton").style.display = "initial"
 	document.getElementById("wordDisplay").innerHTML = ""
 	document.getElementById("wordDisplay2").innerHTML = ""
+	strike = 0
 }
 
 function checkAnswer()
@@ -64,6 +65,16 @@ function checkAnswer()
 	else
 	{		
 		document.getElementById("wordDisplay2").innerHTML = "You answered incorrectly, try again"
+		strike = strike + 1
+		document.getElementById("strikes").innerHTML = "Strikes: " + strike
+		if (strike == 3)
+		{
+			document.getElementById("wordDisplay2").innerHTML = "You have lost. Click to play another to restart."
+			document.getElementById("easy").style.display = "none"
+			document.getElementById("medium").style.display = "none"
+			document.getElementById("hard").style.display = "none"
+			document.getElementById("answerButton").style.display = "none"
+		}
 	}
 }
 
@@ -71,6 +82,7 @@ function easyFourPic()
 {
 	var rand = Math.floor(Math.random() * picsEasy.length)
 	word = picsEasy[rand]
+	strike = 0
 	document.getElementById("Pic1").src = word[0]
 	document.getElementById("Pic2").src = word[1]
 	document.getElementById("Pic3").src = word[2]
@@ -85,6 +97,7 @@ function mediumFourPic()
 {
 	var rand = Math.round(Math.random() * picsMedium.length)
 	word = picsMedium[rand]
+	strike = 0
 	document.getElementById("Pic1").src = word[0]
 	document.getElementById("Pic2").src = word[1]
 	document.getElementById("Pic3").src = word[2]
@@ -99,6 +112,7 @@ function hardFourPic()
 {
 	var rand = Math.round(Math.random() * picsHard.length)
 	word = picsHard[rand]
+	strike = 0
 	document.getElementById("Pic1").src = word[0]
 	document.getElementById("Pic2").src = word[1]
 	document.getElementById("Pic3").src = word[2]
